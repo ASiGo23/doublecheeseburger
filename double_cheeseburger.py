5fa87398d2930c6db277838265803cfa1d92540d
Andre Shi <3262007andre@gmail.com>
2024-07-22 17:32:05 -0400

    Added str() function

--- double_cheeseburger.py
+++ double_cheeseburger.py
@@ -1,49 +1,50 @@
 #!/usr/bin/python
 
 class node():
-    def __init__(self):
+    def __init__(self, info):
+        self.info = info
         self.next       = None
         self.previous   = None
         self.type_next      = None
         self.type_previous  = None
-        return self
     
     def forward(self, 
                 func, 
-                parameter,
-                restrict:type = None, 
-                threads:int = 1, 
-                processes:int = 1):
-        if (restrict == None or isinstance(self, restrict)) and self.type_next != None:
-            func(self,parameter)
-            self.type_next.forward(func, parameter, restrict, threads, processes)
-        elif self.next != None:
-            self.next.forward(func, parameter, restrict, threads, processes)
+                parameter:str,
+                restrict:type = None):
+        func(getattr(self, parameter))
+        if self.next != None:
+            self.next.forward(func, parameter, restrict)
 class double_linked_list():
     def __init__(self, list:list = []):
         self.length = 0
         self.base = None
         self.top = None
-        for node in list:
-            self.append(node)
+        for layer in list:
+            self.append(node(layer))
     
     def append(self, node:node):
         self.length += 1
         if (self.base) == None:
             self.base = node
-        elif (self.base == self.top):
+            return
+        elif (self.top == None):
             self.base.next = node
             node.previous = self.base
+            self.top = node
         else: 
             self.top.next = node
             node.previous = self.top
-
-        self.top = node
-
+            self.top = node
     def forward(self, 
                 func, 
-                parameter,
-                restrict:type = None, 
-                threads:int = 1, 
-                processes:int = 1):
-        self.base.forward(func, parameter, restrict, threads, processes)
+                parameter):
+        self.base.forward(func, parameter)
+
+    def str(self):
+        self.forward(print,"info")
+
+if __name__ == "double_cheeseburger":
+    new = double_linked_list([1,2,3,4])
+    print("printing")
+    new.str()
