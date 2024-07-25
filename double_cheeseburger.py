9610a72e0f57c71a36e24e804d30f377f9a0b576
Andre Shi <3262007andre@gmail.com>
2024-07-25 17:59:49 -0400

    Added typing to the linked list

--- double_cheeseburger.py
+++ double_cheeseburger.py
@@ -11,14 +11,21 @@ class node():
     def forward(self, 
                 func, 
                 parameter:str,
-                restrict:type = None):
+                restrict = None):
         value = func(self, parameter)
-        return(self.next, value)
+        if restrict == None:
+            return(self.next, value)
+        else:
+            return(self.type_next, value)
 class double_linked_list():
     def __init__(self, list:list = []):
         self.length = 0
         self.base = None
         self.top = None
+
+        self.type_base = {}
+        self.type_top = {}
+
         for layer in list:
             self.append(node(layer))
     
@@ -26,7 +33,6 @@ class double_linked_list():
         self.length += 1
         if (self.base) == None:
             self.base = node
-            return
         elif (self.top == None):
             self.base.next = node
             node.previous = self.base
@@ -35,20 +41,36 @@ class double_linked_list():
             self.top.next = node
             node.previous = self.top
             self.top = node
+
+        if type(node.info).__name__ not in self.type_base:
+            self.type_base[type(node.info).__name__] = node
+            return
+        elif type(node.info).__name__ not in self.type_top:
+            self.type_base[type(node.info).__name__].type_next = node
+            node.type_previous = self.type_base[type(node.info).__name__]
+            self.type_top.update({type(node.info).__name__: node})
+        else: 
+            self.type_top[type(node.info).__name__].type_next = node
+            node.type_previous = self.type_top[type(node.info).__name__]
+            self.type_top.update({type(node.info).__name__: node})
     def forward(self, 
                 func, 
-                parameter:str):
+                parameter:str,
+                restriction = None):
         return_values = []
-        next, value = self.base.forward(func, parameter)
+        if restriction == None:
+            next, value = self.base.forward(func, parameter,restriction)
+        else: 
+            next, value = self.type_base[restriction.__name__].forward(func, parameter,restriction)
         return_values.append(value)
         while next != None: 
-            next, value = next.forward(func,parameter)
+            next, value = next.forward(func,parameter,restriction)
             return_values.append(value)
         return return_values
 
-    def list(self):
-        return self.forward(getattr,"info")
+    def list(self, restriction = None):
+        return self.forward(getattr,"info",restriction)
 
 if __name__ == "__main__":
-    new = double_linked_list([1,2,3,4])
-    print(new.list())
+    new = double_linked_list([1,"a",2,"b",3,"c",4,"d"])
+    print(new.list(str))
